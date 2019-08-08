ba303ef7c83c4448a3ee6000da2f15e0d0239b58
make1a <869792674@qq.com>
2019-08-08 14:16:34 +0800

    修复联合填充算法中的bug

--- PaintImageView/PaintImageView.swift
+++ PaintImageView/PaintImageView.swift
@@ -18,16 +18,18 @@ class PaintImageView: UIImageView {
     
     var newColor: UIColor = .red //需要填充的新颜色
     var unchangeableColors = [UIColor]()
-
+    
+    
+    /// 联合填色的种子点
     var seedPointArray = [[CGPoint.init(x: 540, y: 198),
                            CGPoint.init(x: 460, y: 63),
                            CGPoint.init(x: 693.0, y: 217.0),
-                           CGPoint.init(x: 207, y: 614)]]
+                           CGPoint.init(x: 207, y: 614)],
+                          [CGPoint.init(x: 370, y: 362),
+                           CGPoint.init(x: 464, y: 448)]
+    ]
     
     var currentSeedPointArray = [CGPoint]() // 当前关联填充颜色的种子
-    
-    var isFindSeedPoint = false  // 找到关联填色的种子点
-    var isEndCurrentFill = false // 控制结束关联填色
     
     private var pixels:Array<UInt32> //像素点的集合
     private var imageSize = CGSize.zero //图片的大小 width height都是整数
@@ -57,7 +59,7 @@ class PaintImageView: UIImageView {
             }
         }
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if touches.count == 1 , let touch = touches.first{
             let point = touch.location(in: self)
@@ -71,7 +73,7 @@ class PaintImageView: UIImageView {
     /// - Parameters:
     ///   - point: 种子点
     ///   - color: 填充颜色
-    private func floodFill(from point:CGPoint) {
+    private func floodFill(from point:CGPoint, fillOtherSeed: Bool = true) {
         if let imageRef = image?.cgImage  {
             let width = imageRef.width
             let height = imageRef.height
@@ -97,7 +99,7 @@ class PaintImageView: UIImageView {
                 if compareColor(color: colorRgbaValue, otherColor: clearColor.rgbaValue, tolorance: colorTolorance) {
                     return
                 }
-
+                
                 // 不能改变的颜色
                 for color in unchangeableColors {
                     let colorValue = color.rgbaValue
@@ -114,61 +116,64 @@ class PaintImageView: UIImageView {
                 if compareColor(color: colorRgbaValue, otherColor: newColorRgbaValue, tolorance: colorTolorance) {
                     return
                 }
-    
+                
                 seedPointList.push(realPoint)
                 while !seedPointList.isEmpty {
-
                     if let point = seedPointList.pop() {
                         let (xLeft,xRight) = fillLine(seedPoint: point, newColorRgbaValue: newColorRgbaValue,
                                                       originalColorRgbaValue: colorRgbaValue)
                         scanLine(lineNumer: Int(point.y) + 1, xLeft: xLeft, xRight: xRight, originalColorRgbaValue: colorRgbaValue)
                         scanLine(lineNumer: Int(point.y) - 1, xLeft: xLeft, xRight: xRight, originalColorRgbaValue: colorRgbaValue)
-                        
-                        if let scanlie = scanedLines[Int(point.y)] {
-                            if isFindSeedPoint == false {
-                                for  array in seedPointArray {
-                                    for p in array {
-                                        let x = Int(p.x * widthScale)
-                                        let y = Int(p.y * heightScale)
-                                        if (scanlie.lineNumber == y) && (scanlie.xLeft < x && scanlie.xRight > x) {
-                                            isFindSeedPoint = true
-                                            currentSeedPointArray = array
-                                            guard let index = currentSeedPointArray.firstIndex(of: p) else { return  }
-                                            currentSeedPointArray.remove(at: index)
-                                            break
-                                        }
-                                    }
+
+                        // 其他绑定的种子填色
+                        if fillOtherSeed == true {
+                            if let scanlie = scanedLines[Int(point.y)] {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.001) {
+                                    self.fillColorWithOtherSeed(scanline: scanlie, widthScale: widthScale, heightScale: heightScale)
                                 }
                             }
                         }
                     }
                 }
-
+                
+                
                 if let cgImage = context.makeImage() {
-                   let img = UIImage(cgImage: cgImage, scale: image?.scale ?? 2, orientation: .up)
+                    let img = UIImage(cgImage: cgImage, scale: image?.scale ?? 2, orientation: .up)
                     let transition = CATransition.init()
                     transition.duration = 0.3
                     transition.timingFunction = CAMediaTimingFunction.init(name: .linear)
                     transition.type = .fade
                     layer.add(transition, forKey: "colorAnimation")
                     image = img
-                    
-
                 }
-                
-                if isFindSeedPoint == true && isEndCurrentFill == false {
-                    isEndCurrentFill = true
-                    for p in currentSeedPointArray {
-                        floodFill(from: p)
-                    }
-                }
-                
-                isFindSeedPoint = false
-                isEndCurrentFill = false
             }
         }
     }
     
+    /// 填充其他绑定的种子点
+    ///
+    /// - Parameters:
+    ///   - scanline: 扫描线
+    ///   - widthScale: 获取图片上的实际像素点
+    ///   - heightScale: 获取图片上的实际像素点
+    private func fillColorWithOtherSeed(scanline: FillLineInfo, widthScale: CGFloat, heightScale: CGFloat) {
+        
+        for  array in seedPointArray {
+            for p in array {
+                let x = Int(p.x * widthScale)
+                let y = Int(p.y * heightScale)
+                if (scanline.lineNumber == y) && (scanline.xLeft < x && scanline.xRight > x) {
+                    currentSeedPointArray = array
+                    guard let index = currentSeedPointArray.firstIndex(of: p) else { return  }
+                    currentSeedPointArray.remove(at: index)
+                    for p in currentSeedPointArray {
+                        floodFill(from: p, fillOtherSeed: false)
+                    }
+                    
+                }
+            }
+        }
+    }
     /// 通过种子点向左向右填充
     ///
     /// - Parameters:
@@ -176,7 +181,7 @@ class PaintImageView: UIImageView {
     ///   - newColorRgbaValue: 填充的新颜色的值
     ///   - originalColorRgbaValue: 触摸点颜色的值
     /// - Returns: 种子点填充的左右区间 都是闭区间
-   private  func fillLine(seedPoint:CGPoint,newColorRgbaValue:UInt32,originalColorRgbaValue:UInt32) -> (Int,Int) {
+    private  func fillLine(seedPoint:CGPoint,newColorRgbaValue:UInt32,originalColorRgbaValue:UInt32) -> (Int,Int) {
         let imageW = Int(imageSize.width)
         let currntLineMinIndex = Int(seedPoint.y) * imageW
         let currntLineMaxIndex = currntLineMinIndex + imageW
@@ -186,17 +191,17 @@ class PaintImageView: UIImageView {
         if pixels.count >= currntLineMaxIndex {
             var tmpIndex = currentPixelIndex
             while tmpIndex >=  currntLineMinIndex &&
-                  compareColor(color: originalColorRgbaValue, otherColor: pixels[tmpIndex], tolorance: colorTolorance){
-                pixels[tmpIndex] = newColorRgbaValue
-                tmpIndex -= 1
-                xleft -= 1
+                compareColor(color: originalColorRgbaValue, otherColor: pixels[tmpIndex], tolorance: colorTolorance){
+                    pixels[tmpIndex] = newColorRgbaValue
+                    tmpIndex -= 1
+                    xleft -= 1
             }
             tmpIndex = currentPixelIndex + 1
             while tmpIndex < currntLineMaxIndex &&
-                  compareColor(color: originalColorRgbaValue, otherColor: pixels[tmpIndex], tolorance: colorTolorance){
-                pixels[tmpIndex] = newColorRgbaValue
-                tmpIndex += 1
-                xright += 1
+                compareColor(color: originalColorRgbaValue, otherColor: pixels[tmpIndex], tolorance: colorTolorance){
+                    pixels[tmpIndex] = newColorRgbaValue
+                    tmpIndex += 1
+                    xright += 1
             }
         }
         return (xleft + 1,xright)
@@ -210,7 +215,7 @@ class PaintImageView: UIImageView {
     ///   - xLeft: 扫描线的最左侧
     ///   - xRight: 扫描线的最右侧
     ///   - originalColorRgbaValue:  触摸点颜色的值
-   private func scanLine(lineNumer:Int,xLeft:Int,xRight:Int,originalColorRgbaValue:UInt32) {
+    private func scanLine(lineNumer:Int,xLeft:Int,xRight:Int,originalColorRgbaValue:UInt32) {
         if lineNumer < 0 || CGFloat(lineNumer) > imageSize.height - 1{
             return
         }
@@ -245,10 +250,10 @@ class PaintImageView: UIImageView {
             var isFindSeed = false
             //找到此区间的种子点，种子点是存在非边界且未填充的像素点，这些相邻的像素点中最右边的一个
             while currentPixelIndex < currntLineMaxIndex &&
-                  compareColor(color: originalColorRgbaValue, otherColor: pixels[currentPixelIndex], tolorance: colorTolorance) {
-                isFindSeed = true
-                currentPixelIndex += 1
-                xCurrent += 1
+                compareColor(color: originalColorRgbaValue, otherColor: pixels[currentPixelIndex], tolorance: colorTolorance) {
+                    isFindSeed = true
+                    currentPixelIndex += 1
+                    xCurrent += 1
             }
             
             if isFindSeed {
@@ -256,11 +261,11 @@ class PaintImageView: UIImageView {
                 //如果是到区间最右边的倒数第二个点，则需要判断最右边的点是否和originalColorRgbaValue颜色一样，如果一样，则最右边的入栈，否则把上一个点入栈
                 //如果是碰到了边界点退出的，则把当前点的上一个点入栈
                 if compareColor(color: originalColorRgbaValue, otherColor: pixels[currentPixelIndex], tolorance: colorTolorance) &&
-                   currentPixelIndex == currntLineMaxIndex {
+                    currentPixelIndex == currntLineMaxIndex {
                     //若当旧扫描与新扫描的范围关系是左被包含右包含，需要扫描的范围应该是新扫描范围的左点到旧扫描范围的左点的上一个点
                     //此时若扫描范围内的最右点颜色与originalColorRgbaValue一样，并且旧扫描范围的左点的颜色也与originalColorRgbaValue一样，则不需要入栈
                     if leftSpiltIndex == nil ||
-                       !compareColor(color: originalColorRgbaValue, otherColor: pixels[leftSpiltIndex!], tolorance: colorTolorance){
+                        !compareColor(color: originalColorRgbaValue, otherColor: pixels[leftSpiltIndex!], tolorance: colorTolorance){
                         seedPointList.push(CGPoint(x: xCurrent, y: lineNumer))
                     }
                 }else {
@@ -272,10 +277,10 @@ class PaintImageView: UIImageView {
         }
     }
     
-   /// 判断颜色是否是黑色
-   ///
-   /// - Returns: true 是 or false 不是
-   private func isBlackColor(color:UInt32) -> Bool {
+    /// 判断颜色是否是黑色
+    ///
+    /// - Returns: true 是 or false 不是
+    private func isBlackColor(color:UInt32) -> Bool {
         let colorRed = Int((color >> 0) & 0xff)
         let colorGreen = Int((color >> 8) & 0xff)
         let colorBlue = Int((color >> 16) & 0xff)
@@ -289,7 +294,7 @@ class PaintImageView: UIImageView {
         }
         return false
     }
-  
+    
     /// 是否是相似的颜色
     ///
     /// - Returns: true 相似 or false 不相似
@@ -308,9 +313,9 @@ class PaintImageView: UIImageView {
         let otherColorAlpha = Int((otherColor >> 24) & 0xff)
         
         if abs(colorRed - otherColorRed) > tolorance ||
-           abs(colorGreen - otherColorGreen) > tolorance   ||
-           abs(colorBlue - otherColorBlue) > tolorance ||
-           abs(colorAlpha - otherColorAlpha) > tolorance {
+            abs(colorGreen - otherColorGreen) > tolorance   ||
+            abs(colorBlue - otherColorBlue) > tolorance ||
+            abs(colorAlpha - otherColorAlpha) > tolorance {
             return false
         }
         return true
